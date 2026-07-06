e01d70efca285a02faa36d4cf04878d5eb59adaf
Saurav Kumar <sauravkumarcodes@gmail.com>
2026-07-06 14:50:02 +0530

    Time: 4 ms (38.42%), Space: 43.3 MB (41.24%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,23 +1,39 @@
 class Solution {
     public boolean isValid(String s) {
-        HashMap<Character, Character> map = new HashMap<>();
-        map.put('}', '{');
-        map.put(')', '(');
-        map.put(']', '[');
-
         Stack<Character> stack = new Stack<>();
-        for(int i = 0; i<s.length(); i++){
-            char c = s.charAt(i);
-            if(map.containsKey(c)){
-                char e = stack.isEmpty()? '#' : stack.pop();
-                if(e != map.get(c)){
-                    return false;
-                }
+        for (char c:s.toCharArray()){
+            if(c == '('|| c == '{'|| c == '['){
+              stack.push(c);
             }
             else{
-                stack.push(c);
+                if(stack.isEmpty()) return false;
+                char a = stack.pop();
+                if(a !='(' && c==')'||a !='[' && c==']'||a !='{' && c=='}') return false;
             }
         }
         return stack.isEmpty();
     }
 }
+
+
+        // HashMap<Character, Character> map = new HashMap<>();
+        // map.put('}', '{');
+        // map.put(')', '(');
+        // map.put(']', '[');
+
+        // Stack<Character> stack = new Stack<>();
+        // for(int i = 0; i<s.length(); i++){
+        //     char c = s.charAt(i);
+        //     if(map.containsKey(c)){
+        //         char e = stack.isEmpty()? '#' : stack.pop();
+        //         if(e != map.get(c)){
+        //             return false;
+        //         }
+        //     }
+        //     else{
+        //         stack.push(c);
+        //     }
+        // }
+        // return stack.isEmpty();
+//     }
+// }
