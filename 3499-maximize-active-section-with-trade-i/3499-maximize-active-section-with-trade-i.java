87b352850a775b4e34709db917038cf6c043db6a
Saurav Kumar <sauravkumarcodes@gmail.com>
2026-07-21 13:39:16 +0530

    Time: 49 ms (100%), Space: 47.4 MB (98.59%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
@@ -1,30 +1,19 @@
 class Solution {
     public int maxActiveSectionsAfterTrade(String s) {
         int ones = 0;
-        int n = s.length();
-        for(int i = 0; i<n; i++){
-            if(s.charAt(i) == '1') ones++;
-        }
-        List<int[]> block = new ArrayList<>();
-        int i = 0;
-        while(i<n){
-            int j = i;
-            while(j<n && s.charAt(j) == s.charAt(i)){
-                j++;
+        int currZero = 0;
+        int prevZero = 0;
+        int gain = 0;
+        for(char c:s.toCharArray()){
+            if(c=='0') currZero++;
+            else{
+                if(currZero!=0) prevZero = currZero;
+                currZero = 0;
+                ones++;
             }
-            block.add(new int[]{s.charAt(i) -'0', j-i});
-            i=j;
+            gain = Math.max(gain, currZero+prevZero);
         }
-        int gain =0;
-        for(int k = 0; k<block.size(); k++){
-            if(block.get(k)[0] == 1){
-                if(k>0 && block.get(k-1)[0]==0 && k+1<block.size() && block.get(k+1)[0] == 0){
-                    int left = block.get(k-1)[1];
-                    int right = block.get(k+1)[1];
-                    gain = Math.max(gain, left+right);
-                }
-            }
-        }
+        if(gain == currZero || gain == prevZero) return ones;
         return ones+gain;
     }
 }
