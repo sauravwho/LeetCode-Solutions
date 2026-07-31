e98885119138824e7dc186e24e7ecb6bd6972b4b
Saurav Kumar <sauravkumarcodes@gmail.com>
2026-07-31 12:36:30 +0530

    Time: 10 ms (66.76%), Space: 48 MB (52.6%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -5,12 +5,28 @@ class Solution {
         for (char c : word.toCharArray()) {
             freq[c - 'a']++;
         }
-        Arrays.sort(freq);
-        for (int i = 25; i>=0; i--) {
-            if (freq[i] == 0) break;
-            // res += (((25-i) / 8) + 1) * freq[i];
-            res += ((33-i)/8) * freq[i];
+        Integer[] freqArr = new Integer[26];
+        for (int i = 0; i < 26; i++){
+            freqArr[i] = freq[i];
+        }
+        Arrays.sort(freqArr, Collections.reverseOrder());
+        for (int i = 0; i < 26; i++) {
+            if (freqArr[i] == 0) break;
+            res += ((i / 8) + 1) * freqArr[i];
         }
         return res;
+
+        // int[] freq = new int[26];
+        // int res = 0;
+        // for (char c : word.toCharArray()) {
+        //     freq[c - 'a']++;
+        // }
+        // Arrays.sort(freq);
+        // for (int i = 25; i>=0; i--) {
+        //     if (freq[i] == 0) break;
+        //     // res += (((25-i) / 8) + 1) * freq[i];
+        //     res += ((33-i)/8) * freq[i];
+        // }
+        // return res;
     }
 }
