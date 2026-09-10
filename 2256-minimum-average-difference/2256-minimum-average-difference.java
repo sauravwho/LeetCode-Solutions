9c58628062d012984e8d3294a0f429f9e4cb60b9
Saurav Kumar <sauravkumarcodes@gmail.com>
2026-09-10 16:43:12 +0530

    Time: 7 ms (25.75%), Space: 92.8 MB (54.51%) - LeetHub

---
+++ 2256-minimum-average-difference/2256-minimum-average-difference.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minimumAverageDifference(int[] nums) {
+        int n = nums.length;
+        long total = 0;
+        for(int num : nums)   total += num;
+        long left =0;
+        int minIdx = -1;
+        long minDiff = Long.MAX_VALUE;
+        for(int i = 0; i<n; i++){
+            left +=nums[i];
+            long right = total-left;
+            long leftAvg = left/(i+1);
+            long rightAvg = (i==n-1)?0:right/(n-i-1);
+            long curr = Math.abs(leftAvg-rightAvg);
+            if(curr < minDiff){
+                minDiff = curr;
+                minIdx = i;
+            }
+        }
+        return minIdx;
+    }
+}
